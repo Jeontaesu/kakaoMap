137a59a5b1b9f8c908487fc158245b5ee9b98100
jeontaesu1 <jts@frameout.co.kr>
2025-03-07 10:03:22 +0900

    교통량 정보 표시

--- js/kakao.js
+++ js/kakao.js
@@ -1,5 +1,6 @@
+const toggleTraffic = document.querySelector('.toggleTraffic');
 const mapContainer = document.querySelector('#map'); // 지도를 표시할 div 
-const mapOption = { center: new kakao.maps.LatLng(37.51934317566885, 127.05708823178604), level: 2};
+const mapOption = { center: new kakao.maps.LatLng(37.51934317566885, 127.05708823178604), level: 4};
 
 //map instance
 const map = new kakao.maps.Map(mapContainer, mapOption);
@@ -18,3 +19,16 @@ marker.setMap(map);
 
 //add controller layer on map layer
 [mapTypeControl, zoomControl].forEach(el => map.addControl(el));
+
+//toggleTraffic event bind
+toggleTraffic.addEventListener('click', e => {
+  e.target.classList.toggle('on');
+
+  if(e.target.classList.contains('on')) {
+    e.target.innerText = "Traffic OFF"
+    map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC); 
+  }else {
+    e.target.innerText = "Traffic ON";
+    map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC); 
+  }
+})
